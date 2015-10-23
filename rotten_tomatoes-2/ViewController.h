2b70ca4dc9597db6289516f0b65967c9f6135f4f
Matias Arenas <matias@groupon.com>
2015-10-22 23:52:57 -0700

    Initial Commit

---
+++ rotten_tomatoes-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rotten_tomatoes-2
+//
+//  Created by Matias Arenas Sepulveda on 10/22/15.
+//  Copyright © 2015 Matias Arenas Sepulveda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ rotten_tomatoes-2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  rotten_tomatoes-2
+//
+//  Created by Matias Arenas Sepulveda on 10/22/15.
+//  Copyright © 2015 Matias Arenas Sepulveda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
